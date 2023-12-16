2f1509dbe7e89c225aaf2858bcacf623066ff35a
Xcutor <Lekanabass50@gmail.com>
2023-12-15 19:04:43 -0600

    bug fix task 3

--- memory.c
+++ memory.c
@@ -144,25 +144,3 @@ int memcpystr(char **ptr, char **newptr)
 
 	return (0);
 }
-
-
-/**
- * myexists - tests if a pointer exists in an array
- *
- *@allocMem: an array that keeps track of dynamically allocated memory
- *@ptr: the pointer to compare
- *
- *Returns: 0 if the ptr isnt in the list and 1 if it is
- */
-
-int myexists(memStruct *allocMem, void *ptr)
-{
-	int i;
-
-	for (i = 0; (allocMem->memPtr)[i] != NULL; i++)
-	{
-		if (ptr == allocMem->memPtr[i])
-			return 0;
-	}
-	return 1;
-}
